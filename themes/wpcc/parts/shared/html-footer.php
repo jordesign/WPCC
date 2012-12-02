060cf2b742a44a8ad56dbec90673e7cd4f0ae053
jordan <jg@jordesign>
2012-12-03 09:42:10 +1100

    Slight Updates
    Imported content from Testing.
    New images

--- themes/wpcc/parts/shared/html-footer.php
+++ themes/wpcc/parts/shared/html-footer.php
@@ -3,7 +3,10 @@
 	
 </div>
 	<div id="utilities">
-	    <ul>       
+	    <ul>  
+	        <?php if(is_user_logged_in()) { ?>
+	            <li><?php edit_post_link(); ?></li>  
+	        <?php } ?>   
 	        <li id="uSermons"><a href="/wpcc/sermons/">Sermons </a></li>              
 	        <li id="uEvents"><a href="/wpcc/events/">Coming Events </a></li>                            
 	        <li id="uMembers"><a href="/wpcc/members/">Members</a></li>
